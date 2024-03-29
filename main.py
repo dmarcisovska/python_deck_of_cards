82d475b3c99e838b2bbb960657726e5ba5315c9c
Denisa Marcisovska <dmarcisovska@gmail.com>
2024-03-29 11:14:42 -0700

    added __repr__ method to return suit and value

--- main.py
+++ main.py
@@ -2,3 +2,11 @@ class Card:
     def __init__(self, suit, value):
         self._suit = suit
         self._value = value
+
+    def __repr__(self):
+        return f"{self._value} of {self._suit}"
+
+
+class Deck:
+
+
